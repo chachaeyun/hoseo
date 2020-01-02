110e16d84b5076b3a46626183bb174a7a0452ecc
chachaeyun <58388423+chachaeyun@users.noreply.github.com>
2020-01-02 17:29:02 +0900

    Add files via upload

---
+++ js/maingnb.js
@@ -0,0 +1,16 @@
+// gnb
+$(function(){
+  // 마우스 오버시 메뉴 열림
+  $('#main_gnb').mouseover(function(){
+    // class추가되면 보임
+    $('.depth2').addClass('show');
+    $('#ex').addClass('show');
+  });
+
+  // 마우스가 나가면 메뉴 닫힘
+  $('#main_gnb').mouseout(function(){
+    $('.depth2').removeClass('show');
+    $('#ex').removeClass('show');
+  });
+
+});
